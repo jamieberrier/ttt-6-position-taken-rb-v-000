2aecffaf87b66482bd0f21aab3f5a3a7f2a3dbd3
jamieberrier <berrier.jamie@gmail.com>
2019-10-02 19:37:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 require 'pry'
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  binding.pry
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
